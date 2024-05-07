189e737a37d8d0309ae70900ab7a5e3f5d9066d6
0320kangk <0320kangk@naver.com>
2024-05-08 08:31:44 +0900

    game_room.vue modal창 추가

--- src/components/views/Game_rooms.vue
+++ src/components/views/Game_rooms.vue
@@ -6,27 +6,59 @@
       class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
     >
       <!-- 모달 컨테이너 -->
-      <div class="bg-white rounded-lg p-8 w-96">
+      <div class="bg-white rounded-lg p-8 w-2/5">
         <!-- 모달 헤더 -->
         <div class="mb-6 pb-2 border-b-2">
-          <h2 class="text-lg font-semibold">Modal Title</h2>
+          <h2 class="text-lg font-semibold">방 만들기</h2>
         </div>
         <!-- 모달 본문 -->
         <div class="mb-6">
           <form>
-            <div>
-              <span class="">방 제목 </span>
-              <input />
+            <div class="flex items-center">
+              <label for="title" class="w-1/5">방 제목 </label>
+              <input
+                id="title"
+                class="p-2 w-4/5 text-sm border border-gray-500 rounded-lg focus:outline-gray-700"
+                placeholder="방 제목을 입력하세요."
+              />
+            </div>
+            <div class="flex items-center mt-5">
+              <label for="category" class="w-1/5">카테고리 </label>
+              <select
+                id="category"
+                class="p-2 w-4/5 text-sm border border-gray-500 rounded-lg focus:outline-gray-700"
+              >
+                <option value="option1">Option 1</option>
+                <option value="option2">Option 2</option>
+                <option value="option3">Option 3</option>
+              </select>
+            </div>
+            <div class="flex items-center mt-5">
+              <label for="category" class="w-1/5">문제 개수</label>
+              <select
+                id="category"
+                class="p-2 w-4/5 text-sm border border-gray-500 rounded-lg focus:outline-gray-700"
+              >
+                <option value="option1">Option 1</option>
+                <option value="option2">Option 2</option>
+                <option value="option3">Option 3</option>
+              </select>
             </div>
           </form>
         </div>
         <!-- 닫기 버튼 -->
-        <div class="text-right">
+        <div class="flex">
           <button
             @click="closeModal"
-            class="px-4 py-2 bg-gray-300 hover:bg-gray-400 rounded-lg"
+            class="px-4 py-2 w-1/2 mr-2 bg-yellow-400 hover:bg-yellow-500 rounded-lg"
           >
-            Close
+            방 만들기
+          </button>
+          <button
+            @click="closeModal"
+            class="px-4 py-2 w-1/2 ml-2 bg-gray-300 hover:bg-gray-400 rounded-lg"
+          >
+            취소
           </button>
         </div>
       </div>
