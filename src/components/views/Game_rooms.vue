627648b8dee8bf4a55fa4bf2aad1bba4ff05a96f
0320kangk <0320kangk@naver.com>
2024-05-08 13:50:44 +0900

    game_rooms 안의 방 모양 만들기

--- src/components/views/Game_rooms.vue
+++ src/components/views/Game_rooms.vue
@@ -1,4 +1,43 @@
 <template>
+  <!-- <table class="min-w-full divide-y divide-gray-200">
+          <thead>
+            <tr>
+              <th
+                class="px-6 py-4 bg-blue-200 text-left rounded-tl-lg text-xl leading-4 font-bold text-gray-500 uppercase tracking-wider"
+              >
+                카테고리
+              </th>
+              <th
+                class="px-6 py-3 bg-blue-200 text-left text-xl leading-4 font-bold text-gray-500 uppercase tracking-wider"
+              >
+                상태
+              </th>
+              <th
+                class="px-6 py-3 bg-blue-200 text-left text-xl leading-4 font-bold text-gray-500 uppercase tracking-wider"
+              >
+                방 제목
+              </th>
+              <th
+                class="px-6 py-3 bg-blue-200 text-left text-xl rounded-tr-lg leading-4 font-bold text-gray-500 uppercase tracking-wider"
+              >
+                인원
+              </th>
+            </tr>
+          </thead>
+          <tbody class="bg-white divide-y divide-gray-200">
+            <tr
+              v-for="(item, index) in tableData"
+              :key="index"
+              class="hover:bg-gray-300"
+            >
+              <td class="px-6 py-3">{{ item.category }}</td>
+              <td class="px-6 py-3">{{ item.status }}</td>
+              <td class="px-6 py-3">{{ item.title }}</td>
+              <td class="px-6 py-3">{{ item.members }}</td>
+            </tr>
+          </tbody>
+        </table> -->
+
   <div>
     <!-- 배경 오버레이 -->
     <div
@@ -66,8 +105,8 @@
   </div>
   <div class="layout-default">
     <div class="grid grid-cols-12">
-      <div class="col-span-2">
-        <div class="w-full p-4 bg-gray-100 rounded-3xl shadow-2xl">
+      <div class="col-span-12 sm:col-span-2 m-3">
+        <div class="w-full p-4 bg-gray-100 rounded-3xl shadow-2xl gap-x-4">
           <div
             class="p-3 my-5 font-semibold bg-yellow-400 hover:bg-yellow-400 rounded-xl"
           >
@@ -91,6 +130,53 @@
           </button>
         </div>
       </div>
+      <div
+        class="col-span-12 m-3 p-3 sm:col-span-9 w-full bg-gray-100 rounded-2xl"
+      >
+        <div class="grid grid-cols-2">
+          <div
+            v-for="(item, index) in room_data"
+            :key="index"
+            class="col-span-2 m-3 sm:col-span-1 bg-blue-200 p-4 rounded-3xl"
+          >
+            <div class="p-3 bg-blue-300 text-sm rounded-xl hover:bg-gray-400">
+              <div
+                class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-red-200 text-gray-800"
+              >
+                방 제목
+              </div>
+              <span> 크레이지 아케이드</span>
+              <div class="mt-2">
+                <div
+                  class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-yellow-200 text-gray-800"
+                >
+                  카테고리
+                </div>
+                <span>카테고리 이름</span>
+              </div>
+              <div class="mt-2 flex justify-between">
+                <div>
+                  <div
+                    class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-200 text-gray-800"
+                  >
+                    방 상태
+                  </div>
+                  <span> 대기</span>
+                </div>
+
+                <div>
+                  <div
+                    class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-blue-500 text-gray-800"
+                  >
+                    인원수
+                  </div>
+                  <span>1/4</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -101,6 +187,22 @@ export default {
   data() {
     return {
       isOpen: false, // 모달이 열려있는지 여부
+      room_data: [
+        { category: "IT", status: "활성", title: "Vue.js 강의", members: 25 },
+        {
+          category: "디자인",
+          status: "비활성",
+          title: "UI/UX 디자인 토론",
+          members: 12,
+        },
+        {
+          category: "게임",
+          status: "활성",
+          title: "모바일 게임 개발",
+          members: 8,
+        },
+        // 원하는 만큼 데이터를 추가할 수 있습니다.
+      ],
     };
   },
   methods: {
