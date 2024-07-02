1a3847212d657e819db14e53fcb4f74c25a853f7
0320kangk <0320kangk@naver.com>
2024-07-02 22:17:13 +0900

    방 카테고리 option 변경

--- src/components/views/GameRooms.vue
+++ src/components/views/GameRooms.vue
@@ -71,7 +71,7 @@
               >
                 <option hidden disabled value="">카테고리를 선탁하세요.</option>
 
-                <option value="option1">Option 1</option>
+                <option value="Spring framework">Spring</option>
                 <option value="option2">Option 2</option>
                 <option value="option3">Option 3</option>
               </select>
